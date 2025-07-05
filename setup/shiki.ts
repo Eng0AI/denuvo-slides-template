f8a1759ea81e82124dc2dc92a8906fff57cb0be5
momo5502 <mauriceheumann@gmail.com>
2025-07-05 16:35:19 +0200

    Progress

---
+++ setup/shiki.ts
@@ -0,0 +1,13 @@
+import { defineShikiSetup } from "@slidev/types";
+
+export default defineShikiSetup(() => {
+  return {
+    themes: {
+      dark: "material-theme-darker",
+      light: "min-light",
+    },
+    transformers: [
+      // ...
+    ],
+  };
+});
